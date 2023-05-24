9496778b4c70d9b3f52ef15d958e280c536891ac
“dagiman-droid” <dagijhone@gmail.com>
2023-05-24 03:42:49 -0700

    minor bug

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,50 +1,27 @@
 #!/usr/bin/python3
-# script to gather todo data from an API and write to JSON file
-import json
-import requests
-import sys
+"""
+Return information for a given employee about his/her TODO list progress
+"""
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
 
+    DONE_TASKS = 0
+    ALL_TASKS = 0
+    csvList = []
 
-def get_username(base_url, user_id):
-    """Gets username
-       Args:
-           base_url (str): base url for API
-           user_id (str): user id number
-       Returns: username
-    """
-    response = requests.get(
-        "{}users/{}".format(base_url, user_id))
-    usr_dict = response.json()
-    return usr_dict['username']
+    URL_FOR_USERS = 'https://jsonplaceholder.typicode.com/users/{0}'.\
+        format(sys.argv[1])
+    URL_FOR_TODOS = 'https://jsonplaceholder.typicode.com/todos'
+    r_for_users = requests.get(URL_FOR_USERS)
+    r_for_todos = requests.get(URL_FOR_TODOS)
 
-
-def get_todo_list(base_url, user_id):
-    """Gets todo list
-       Args:
-           base_url (str): base url for API
-           user_id (str): user id number
-       Returns: list of todo items (dicts)
-    """
-    response = requests.get(
-        "{}users/{}/todos".format(base_url, user_id))
-    return response.json()
-
-
-if __name__ == '__main__':
-    user_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com/'
-
-    uname = get_username(base_url, user_id)
-    todo_list = get_todo_list(base_url, user_id)
-
-    with open('{}.json'.format(user_id), 'w') as f:
-        todo_dict = {}
-        todo_dict[sys.argv[1]] = []
-        for todo in todo_list:
-            task = {}
-            task['task'] = todo['title']
-            task['completed'] = todo['completed']
-            task['username'] = uname
-            todo_dict[sys.argv[1]].append(task)
-
-        f.write(json.dumps(todo_dict))
+    name = r_for_users.json().get('name')
+    user_name = r_for_users.json().get('username')
+    todos = r_for_todos.json()
+    with open(sys.argv[1] + '.json', 'w+') as f:
+        json.dump({sys.argv[1]: [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user_name} for todo in todos]}, f))

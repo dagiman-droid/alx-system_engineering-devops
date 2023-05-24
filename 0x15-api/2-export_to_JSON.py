5fb766f1ff7261923cdabdda91f1502e4c4f93bf
“dagiman-droid” <dagijhone@gmail.com>
2023-05-24 03:50:19 -0700

    hd

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,26 +1,29 @@
+#!/usr/bin/python3
 import json
+import requests
+from sys import argv
 
-def export_tasks_to_json(user_id, tasks):
-    data = {user_id: []}
+
+def export_tasks_to_json(user_id):
+    response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    tasks = response.json()
+
+    user_tasks = []
     for task in tasks:
-        data[user_id].append({
-            "task": task["title"],
-            "completed": task["completed"],
-            "username": task["username"]
-        })
-    
+        if task["userId"] == int(user_id):
+            user_tasks.append({
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": task["username"]
+            })
+
+    data = {user_id: user_tasks}
     file_name = f"{user_id}.json"
     with open(file_name, "w") as json_file:
         json.dump(data, json_file)
 
-# Example usage
-user_id = "USER123"
-tasks = [
-    {"title": "Task 1", "completed": False, "username": "John"},
-    {"title": "Task 2", "completed": True, "username": "John"},
-    {"title": "Task 3", "completed": True, "username": "John"},
-    {"title": "Task 4", "completed": False, "username": "John"}
-]
+# Get the user_id from the command line argument
+user_id = argv[1]
 
-export_tasks_to_json(user_id, tasks)
+export_tasks_to_json(user_id)
 
